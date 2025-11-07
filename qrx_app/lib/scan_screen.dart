38c85c01be0a06772985ae6c7e2ca11b2cfd121b
Mary Vincent Lopez <lopezmaryvincent04@gmail.com>
2025-11-07 21:24:16 +0800

    Added scan screen feature

---
+++ qrx_app/lib/scan_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
+
+class ScanScreen extends StatefulWidget {
+  const ScanScreen({super.key});
+
+  @override
+  State<ScanScreen> createState() => _ScanScreenState();
+}
+
+class _ScanScreenState extends State<ScanScreen> {
+  final MobileScannerController cameraController = MobileScannerController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Scan Medicine QR")),
+      body: MobileScanner(
+        controller: cameraController,
+        onDetect: (capture) {
+          final List<Barcode> barcodes = capture.barcodes;
+          if (barcodes.isNotEmpty) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Scanned: ${barcodes.first.rawValue}')),
+            );
+          }
+        },
+      ),
+    );
+  }
+}
